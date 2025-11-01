c23bd3b8ce667e85ee3e7759ee893316b4372736
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 20:49:06 +0700

    create ALG model

---
+++ app/Models/Alg.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class Alg extends Model
+{
+    /** @use HasFactory<\Database\Factories\AlgFactory> */
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function nutritions(): MorphToMany
+    {
+        return $this->morphToMany(Nutrition::class, 'nutrientable')
+            ->withPivot(['value']);
+    }
+}
