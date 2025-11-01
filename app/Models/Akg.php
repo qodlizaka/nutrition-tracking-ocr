c6235a254860b18744b70a011ad272fdd18282e8
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 20:46:25 +0700

    add nutritions morph relationship

--- app/Models/Akg.php
+++ app/Models/Akg.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enum\Gender;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Akg extends Model
 {
@@ -18,5 +19,11 @@ class Akg extends Model
         return [
             'gender' => Gender::class,
         ];
+    }
+
+    public function nutritions(): MorphToMany
+    {
+        return $this->morphToMany(Nutrition::class, 'nutrientable')
+            ->withPivot(['value', 'percentage']);
     }
 }
