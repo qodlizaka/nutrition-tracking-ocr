78275e09cfd077ccdb9b31c9652e0f3effb9f03e
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 20:23:29 +0700

    add morph relation to category

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Food extends Model
 {
@@ -11,4 +12,9 @@ class Food extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function categories(): MorphToMany
+    {
+        return $this->morphToMany(Category::class, 'categorizeable');
+    }
 }
