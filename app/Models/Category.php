8822417e9920a6240303632c744776205423592c
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 19:48:55 +0700

    rename foodcategory to just category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FoodCategory extends Model
+class Category extends Model
 {
-    /** @use HasFactory<\Database\Factories\FoodCategoryFactory> */
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
     protected $guarded = ['id'];
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\FoodCategory>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
-class FoodCategoryFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
