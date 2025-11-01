9dfd70cf6937b30382827ba0f69e4a09ba710df7
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 20:23:15 +0700

    fix typo

--- database/migrations/2025_11_01_125416_create_categorizables_table.php
+++ database/migrations/2025_11_01_125416_create_categorizables_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categorizables', function (Blueprint $table) {
+        Schema::create('categorizeables', function (Blueprint $table) {
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->morphs('categorizable');
+            $table->morphs('categorizeable');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categorizables');
+        Schema::dropIfExists('categorizeables');
     }
 };
