185b7faf1dc2339f4b7cd956b942c8d3cbbd4133
qodlizaka <qodlizaka513@gmail.com>
2025-11-01 20:26:22 +0700

    define polymorphic table for nutrition

---
+++ database/migrations/2025_11_01_132437_create_nutritionables_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nutritionables', function (Blueprint $table) {
+            $table->foreignId('nutrition_id')->constrained()->cascadeOnDelete();
+            $table->morphs('nutritionable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nutritionables');
+    }
+};
