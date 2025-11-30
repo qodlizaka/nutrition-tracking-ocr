39c42034fbd60297d855dda769a3a39b0c97c8f6
qodlizaka <qodlizaka513@gmail.com>
2025-11-30 20:31:16 +0700

    define nutrition seeder

--- database/seeders/NutritionSeeder.php
+++ database/seeders/NutritionSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Enum\NutritionCategory;
+use App\Enum\NutritionGroup;
+use App\Models\Category;
+use App\Models\Nutrition;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class NutritionSeeder extends Seeder
 {
@@ -12,6 +17,18 @@ class NutritionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jsonPath = database_path('data/nutritions_data.json');
+        $json = File::get($jsonPath);
+
+        $groups = array_flip(NutritionGroup::array());
+        $categories = array_flip(NutritionCategory::array());
+
+        collect(json_decode($json, true))
+            ->map(fn($n) => [
+                ...$n,
+                'group' => NutritionGroup::from($groups[$n['group']]),
+                'category' => NutritionCategory::from($categories[ucfirst($n['category'])]),
+            ])
+            ->each(fn(array $n) => Nutrition::create($n));
     }
 }
