d22a4c02db30d1b2ec5a29a43ee4d9bf239c8e0f
qodlizaka <qodlizaka513@gmail.com>
2025-11-30 21:19:29 +0700

    define alg seeder

--- database/seeders/AlgSeeder.php
+++ database/seeders/AlgSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Alg;
+use App\Models\Nutrition;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class AlgSeeder extends Seeder
 {
@@ -12,6 +15,30 @@ class AlgSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jsonPath = database_path('data/alg_data.json');
+        $json = File::get($jsonPath);
+        $nutritions = Nutrition::all()->keyBy('name');
+
+        collect(json_decode($json, true))
+            ->map(fn($data) => [
+                ...$data,
+                'nutritions' => collect([
+                        ...$data['nutritions'],
+                        [
+                            'name' => 'energy',
+                            'value' => $data['energy'],
+                        ],
+                    ])
+                    ->keyBy(fn(array $data): int => $nutritions->get($data['name'])->id)
+                    ->map(fn(array $data): array => ['value' => $data['value']]),
+            ])
+            ->each(function(array $data) {
+                $alg = Alg::create([
+                    'name' => $data['name'],
+                    'energy' => $data['energy'],
+                ]);
+
+                $alg->nutritions()->attach($data['nutritions']);
+            });
     }
 }
