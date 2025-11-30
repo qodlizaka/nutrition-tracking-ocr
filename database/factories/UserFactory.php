7d31520072ae9d67080bfa0f962c06fc6763a8e7
qodlizaka <qodlizaka513@gmail.com>
2025-11-30 18:06:56 +0700

    add birth date and gender

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enum\Gender;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -29,6 +30,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'date_of_birth' => now()->subdays(rand(1, 365))->subyears(rand(12, 50)),
+            'gender' => fake()->randomElement(Gender::cases()),
         ];
     }
 
