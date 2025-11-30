3a09fd1b170d3b3eff6b483600c03b36b40ec9fa
qodlizaka <qodlizaka513@gmail.com>
2025-11-30 20:38:06 +0700

    change description from string to text

--- database/migrations/2025_11_01_120947_create_nutrition_table.php
+++ database/migrations/2025_11_01_120947_create_nutrition_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('unit');
-            $table->string('description');
+            $table->text('description');
             $table->unsignedTinyInteger('category');
             $table->unsignedTinyInteger('group');
             $table->timestamps();
