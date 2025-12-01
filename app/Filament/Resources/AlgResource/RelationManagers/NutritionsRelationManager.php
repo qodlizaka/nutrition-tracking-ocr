289070463ff7ca6efb76dd550f08a5ed735348d2
qodlizaka <qodlizaka513@gmail.com>
2025-12-01 18:35:31 +0700

    add nutrition relation manager for nutrition

---
+++ app/Filament/Resources/AlgResource/RelationManagers/NutritionsRelationManager.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources\AlgResource\RelationManagers;
+
+use App\Models\Nutrition;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables;
+use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NutritionsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'nutritions';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('value')
+                    ->required()
+                    ->numeric()
+                    ->maxLength(255)
+                    ->translateLabel(),
+            ])
+            ->columns(1);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->translateLabel(),
+                Tables\Columns\TextColumn::make('value')
+                    ->translateLabel(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make()
+                    ->form(fn (AttachAction $action): array => [
+                        $action->getRecordSelect()
+                            ->options(Nutrition::pluck('name', 'id')),
+                        Forms\Components\TextInput::make('value')
+                            ->required()
+                            ->numeric()
+                            ->maxLength(255)
+                            ->translateLabel(),
+                    ])
+                    ->modalWidth(MaxWidth::Small),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->modalWidth(MaxWidth::Small),
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ]);
+    }
+}
