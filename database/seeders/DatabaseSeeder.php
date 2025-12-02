a7069f1f55f0e1e51ea702fb23466898a2c78b55
qodlizaka <qodlizaka513@gmail.com>
2025-12-02 19:48:55 +0700

    call alg seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             NutritionSeeder::class,
             AlgSeeder::class,
+            AkgSeeder::class,
         ]);
     }
 }
