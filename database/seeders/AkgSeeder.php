b80bef3251fe8d887a7f7c1399e7c928acd7e549
qodlizaka <qodlizaka513@gmail.com>
2025-12-02 19:48:48 +0700

    seed initial alg data

--- database/seeders/AkgSeeder.php
+++ database/seeders/AkgSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enum\Gender;
+use App\Models\Akg;
+use App\Models\Nutrition;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class AkgSeeder extends Seeder
 {
@@ -12,6 +16,32 @@ class AkgSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $nutritions = Nutrition::pluck('id', 'name');
+        $genders = array_flip(Gender::array());
+
+        $jsonPath = database_path('data/akg_data.json');
+        $data = json_decode(File::get($jsonPath), true);
+
+        collect($data)->map(function (array $data) use ($nutritions, $genders): Akg {
+            $mappedNutritions = collect($data['nutritions'])
+                ->mapWithKeys(function (int $amount, string $name) use ($nutritions): array {
+                    $id = $nutritions->get($name);
+
+                    if (!$id) return [];
+
+                    return [$id => ['value' => $amount]];
+                });
+
+            unset($data['nutritions']);
+
+            $gender = ucfirst($data['gender']);
+            $akg = Akg::query()->create([
+                ...$data,
+                'gender' => empty($gender) ? null : Gender::from($genders[$gender]),
+            ]);
+            $akg->nutritions()->attach($mappedNutritions);
+
+            return $akg;
+        });
     }
 }
