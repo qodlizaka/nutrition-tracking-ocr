f1c77325c3c64e64884ec085bc989083762fd8d9
qodlizaka <qodlizaka513@gmail.com>
2025-12-11 09:08:21 +0700

    change hasfactory definition

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use Database\Factories\FoodFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Food extends Model
 {
-    /** @use HasFactory<FoodFactory> */
+    /** @use HasFactory<\Database\Factories\FoodFactory> */
     use HasFactory;
 
     protected $guarded = ['id'];
