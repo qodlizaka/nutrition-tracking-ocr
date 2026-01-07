30081dd6d705d5948e53c61dd80d477daedba281
qodlizaka <qodlizaka513@gmail.com>
2026-01-07 18:41:32 +0700

    change comment

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\FoodFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Food extends Model
 {
-    /** @use HasFactory<\Database\Factories\FoodFactory> */
+    /** @use HasFactory<FoodFactory> */
     use HasFactory;
 
     protected $guarded = ['id'];
