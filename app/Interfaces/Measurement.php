070ff460775b0eb6c19587810b9e527fe3c8cd12
qodlizaka <qodlizaka513@gmail.com>
2026-01-09 20:36:12 +0700

    create interface for measurement

---
+++ app/Interfaces/Measurement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Interfaces;
+
+interface Measurement
+{
+    public function getName(): string;
+    public function getMultiplier(): float;
+    public function getUnit(): string;
+    public function getIcon(): string;
+}
