21d3d2289d93de7257eb5e42357d89b2dcece48c
qodlizaka <qodlizaka513@gmail.com>
2026-01-09 12:54:37 +0700

    modify to looks like flux

--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -1,24 +1,32 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex gap-2 items-center justify-between">
+    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
 
+        {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <span class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-600 bg-white border border-gray-300 cursor-not-allowed leading-5 rounded-md dark:text-gray-300 dark:bg-gray-700 dark:border-gray-600">
+            <flux:button disabled variant="subtle" icon="chevron-left">
                 {!! __('pagination.previous') !!}
-            </span>
+            </flux:button>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-700 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-gray-200">
+            <flux:button
+                href="{{ $paginator->previousPageUrl() }}"
+                variant="subtle"
+                icon="chevron-left">
                 {!! __('pagination.previous') !!}
-            </a>
+            </flux:button>
         @endif
 
+        {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-700 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-800 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-gray-200">
+            <flux:button
+                href="{{ $paginator->nextPageUrl() }}"
+                variant="subtle"
+                icon-trailing="chevron-right">
                 {!! __('pagination.next') !!}
-            </a>
+            </flux:button>
         @else
-            <span class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-600 bg-white border border-gray-300 cursor-not-allowed leading-5 rounded-md dark:text-gray-300 dark:bg-gray-700 dark:border-gray-600">
+            <flux:button disabled variant="subtle" icon-trailing="chevron-right">
                 {!! __('pagination.next') !!}
-            </span>
+            </flux:button>
         @endif
 
     </nav>
