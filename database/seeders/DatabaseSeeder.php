136abec2441bc72c97fa0579f9918b8c7912aaf5
qodlizaka <qodlizaka513@gmail.com>
2026-01-12 21:51:00 +0700

    register intake seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             AkgSeeder::class,
             FoodSeeder::class,
             UserSeeder::class,
+            IntakeSeeder::class,
         ]);
     }
 }
