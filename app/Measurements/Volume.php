06b813bf2acd79e3bbbe11e73571265b6eded331
qodlizaka <qodlizaka513@gmail.com>
2026-01-12 18:36:53 +0700

    add base multiplier

--- app/Measurements/Volume.php
+++ app/Measurements/Volume.php
@@ -9,12 +9,20 @@ class Volume extends Base
     public string $name = 'Volume';
     public string $unit = 'ml';
     public string $icon = 'scale';
+    public float $baseMultiplier = 1;
 
     public function getMultiplier(): float
     {
         if ($this->food->unit !== $this->unit)
             return 0;
 
-        return $this->value / $this->food->total_servings; # times the density of the food
+        return ($this->value * $this->baseMultiplier) / $this->food->total_servings; # times the density of the food
+    }
+
+    public function setBaseMultiplier(float $baseMultiplier): static
+    {
+        $this->baseMultiplier = $baseMultiplier;
+
+        return $this;
     }
 }
