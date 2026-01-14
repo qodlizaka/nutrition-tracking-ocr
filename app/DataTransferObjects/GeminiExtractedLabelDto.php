697e56ae48e4dc8073a43114e9660a2ba11e6b0c
qodlizaka <qodlizaka513@gmail.com>
2026-01-14 14:50:41 +0700

    create dto class to transform gemini response

---
+++ app/DataTransferObjects/GeminiExtractedLabelDto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+use Illuminate\Support\Str;
+
+class GeminiExtractedLabelDto
+{
+    /**
+     * Create a new class instance.
+     * "serving_size" => 200,
+     * "serving_unit" => "ml",
+     * "serving_per_package" => 5,
+     * "nutrition" => [
+     */
+    public function __construct(
+        public int $servingSize,
+        public string $servingUnit,
+        public int $servingPerPackage,
+        public array $nutritions
+    )
+    {
+    }
+
+    public static function fromApiResult(array $result): GeminiExtractedLabelDto
+    {
+        return new self(
+            $result['serving_size'],
+            $result['serving_unit'],
+            $result['serving_per_package'],
+            collect($result['nutritions'])
+                ->map(fn($nutri, $name) => [
+                    'name' => Str::replace('_', ' ', $name),
+                    ...$nutri,
+                ])
+                ->values()
+                ->toArray(),
+        );
+    }
+}
