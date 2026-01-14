b4f44c3001d83d5dd3a52b3bdb4ac8862f28aca7
qodlizaka <qodlizaka513@gmail.com>
2026-01-14 13:38:05 +0700

    create action to calculate BMR

---
+++ app/Actions/CalculateBmrAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\User;
+
+class CalculateBmrAction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __invoke(User $user): float
+    {
+        $age = $user->date_of_birth->age;
+        $weight = $user->detail->weight;
+        $height = $user->detail->height;
+
+        if ($user->gender->isMale())
+            return 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age);
+
+        return 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age);
+    }
+}
