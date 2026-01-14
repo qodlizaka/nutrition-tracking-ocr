06fff83fd12c53077f81b194947be44abc86d27c
qodlizaka <qodlizaka513@gmail.com>
2026-01-14 13:41:45 +0700

    add akg_id to user details instead

--- database/migrations/2026_01_14_062635_add_akg_id_column_to_users_table.php
+++ database/migrations/2026_01_14_062635_add_akg_id_column_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('user_details', function (Blueprint $table) {
             $table->foreignId('akg_id')->nullable()->constrained()->nullOnDelete();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('user_details', function (Blueprint $table) {
             $table->dropColumn('akg_id');
         });
     }
