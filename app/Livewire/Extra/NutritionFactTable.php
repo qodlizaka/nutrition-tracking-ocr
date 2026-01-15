2192872e59aea7ddca9c50a0a5cab0464d4579b8
qodlizaka <qodlizaka513@gmail.com>
2026-01-15 10:13:27 +0700

    add compability to food nutrition percentage

--- app/Livewire/Extra/NutritionFactTable.php
+++ app/Livewire/Extra/NutritionFactTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire\Extra;
 
+use App\Models\Akg;
 use App\Models\Food;
 use App\Models\Nutrition;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
@@ -20,6 +22,8 @@ class NutritionFactTable extends Component
 
     public bool $editable = false;
 
+    public Akg $userAkg;
+
     public function mount(Food $food, float $multiplier = 1.0, bool $editable = false): void
     {
         $this->food = $food;
@@ -28,17 +32,28 @@ class NutritionFactTable extends Component
         $this->multiplier = $this->editable ? 1.0 : $multiplier;
 
         $this->nutritions = $food->nutritions;
-        // dd($this->nutritionMap->pluck('pivot')->toArray());
 
-        if ($this->editable) {
-            $this->hydrateForm();
-        }
+        // if ($this->editable) {
+        $this->hydrateForm();
+        // }
+
+        $this->userAkg = Auth::user()
+            ->load(['detail.akg.nutritions'])
+            ->detail
+            ->akg;
+
+        $this->userAkg->setRelation(
+            'nutritions',
+            $this->userAkg->nutritions->keyBy('id')
+        );
     }
 
     public function hydrateForm(): void
     {
         foreach ($this->food->nutritions as $nutrition) {
-            $this->form[$nutrition->id] = $nutrition->pivot->value;
+            $this->form[$nutrition->id] = !is_null($nutrition->pivot->percentage)
+                ? ['percentage' => $nutrition->pivot->percentage]
+                : ['value' => $nutrition->pivot->value];
         }
     }
 
@@ -63,25 +78,40 @@ class NutritionFactTable extends Component
 
     public function calculate(mixed $itemOrValue): float
     {
-        if ($this->editable && \is_object($itemOrValue)) {
-            return (float) ($this->form[$itemOrValue->id] ?? 0);
+        if (! \is_object($itemOrValue)) {
+            return 0;
         }
 
-        $value = \is_object($itemOrValue)
-            ? ($itemOrValue->pivot->value ?? 0)
-            : ($itemOrValue ?? 0);
+        if ($this->editable) {
+            return array_key_first($this->form[$itemOrValue->id]) === 'percentage'
+                ? (float) $this->form[$itemOrValue->id]['percentage']
+                : (float) $this->form[$itemOrValue->id]['value'];
+        }
 
-        return round($value * $this->multiplier, 1);
+        $value = $itemOrValue->pivot?->percentage !== null
+            ? ($itemOrValue->pivot->percentage ?? 0)
+            : ($itemOrValue->pivot->value ?? 0);
+
+        return $itemOrValue->pivot?->percentage !== null
+            ? round(($value / 100 * ($this->userAkg->nutritions->get($itemOrValue->id)?->pivot->value ?? 0)) * $this->multiplier, 4)
+            : round($value * $this->multiplier, 4);
     }
 
     public function save(): void
     {
         $syncData = [];
-        foreach ($this->form as $id => $value) {
-            $syncData[$id] = ['value' => $value];
+        foreach ($this->form as $id => $data) {
+            $pivot = ['value' => null, 'percentage' => null];
+
+            $syncData[$id] = array_key_first($data) === 'percentage'
+                ? [...$pivot, 'percentage' => $data['percentage']]
+                : [...$pivot, 'value' => $data['value']];
         }
 
         $this->food->nutritions()->sync($syncData);
+        $this->food->load('nutritions');
+
+        $this->hydrateForm();
 
         $this->dispatch('nutritions-saved');
     }
