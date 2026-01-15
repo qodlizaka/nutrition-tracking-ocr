5ae1c7d51671b87056363acafbe55cd9b5809e62
qodlizaka <qodlizaka513@gmail.com>
2026-01-15 19:22:42 +0700

    add user id foreign key

--- database/migrations/2025_11_01_123554_create_food_table.php
+++ database/migrations/2025_11_01_123554_create_food_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('food', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('image')->nullable();
             $table->float('total_servings', 4);
