2a4991c8204eead09d916fe95499b3fcf2f50297
qodlizaka <qodlizaka513@gmail.com>
2026-01-15 20:32:37 +0700

    add commented actual gemini request

--- app/Livewire/FoodLabel/Capture.php
+++ app/Livewire/FoodLabel/Capture.php
@@ -44,6 +44,7 @@ class Capture extends Component
             $filename = 'images/food-labels/' . Str::random(40) . '.jpg';
             Storage::disk('public')->put($filename, $finalImageBinary);
 
+            // $apiResult = app(ExtractNutritionFactsAction::class)(base64_encode($image));
             $apiResult = config('gemini.schemas.sample');
             $extractedLabel = GeminiExtractedLabelDto::fromApiResult($apiResult);
 
