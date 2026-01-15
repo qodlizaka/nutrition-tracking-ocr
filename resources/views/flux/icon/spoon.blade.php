08d1a96f4f66b1da65c8730ccf33a54457b772d7
qodlizaka <qodlizaka513@gmail.com>
2026-01-15 10:31:46 +0700

    install new icon

---
+++ resources/views/flux/icon/spoon.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'variant' => 'outline',
+])
+
+@php
+if ($variant === 'solid') {
+    throw new \Exception('The "solid" variant is not supported in Lucide.');
+}
+
+$classes = Flux::classes('shrink-0')
+    ->add(match($variant) {
+        'outline' => '[:where(&)]:size-6',
+        'solid' => '[:where(&)]:size-6',
+        'mini' => '[:where(&)]:size-5',
+        'micro' => '[:where(&)]:size-4',
+    });
+
+$strokeWidth = match ($variant) {
+    'outline' => 2,
+    'mini' => 2.25,
+    'micro' => 2.5,
+};
+@endphp
+
+@svg('fas-spoon')
