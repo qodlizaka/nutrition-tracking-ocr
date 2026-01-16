61ca19271596ea8de2d86b1b9b9bca68b1df94b7
qodlizaka <qodlizaka513@gmail.com>
2026-01-16 21:22:40 +0700

    add hex to rgba helper

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,13 @@ import colors from 'tailwindcss/colors';
 Chart.register(annotationPlugin);
 window.Chart = Chart;
 window.twColors = colors;
+
+function hexToRgba(hex, alpha) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+window.hexToRgba = hexToRgba;
