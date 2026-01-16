418a7b2617d6789102d5cc8ae7d726e003266bbf
qodlizaka <qodlizaka513@gmail.com>
2026-01-16 21:12:02 +0700

    make the value respect the user akg

--- database/seeders/IntakeSeeder.php
+++ database/seeders/IntakeSeeder.php
@@ -16,16 +16,19 @@ class IntakeSeeder extends Seeder
     {
         $nutritions = Nutrition::all();
         $testUser = User::findOrFail(3);
+        $userAkg = $testUser->detail->akg->nutritions->keyBy('id');
 
-        Intake::factory(1500)
+        $intakeCount = 1500;
+
+        Intake::factory($intakeCount)
             ->state(['user_id' => $testUser->id])
             ->create()
-            ->each(function ($intake) use ($nutritions) {
+            ->each(function ($intake) use ($nutritions, $userAkg, $intakeCount) {
                 $intake->nutritions()->attach(
                     $nutritions->shuffle()
                         ->take(rand(15, 25))
                         ->mapWithKeys(fn ($n) => [$n->id => [
-                            'value' => fake()->randomFloat(2, 10, 900),
+                            'value' => ($userAkg->get($n->id)?->pivot->value ?? rand(100, 500)) / ($intakeCount / rand(80, 100)),
                             'created_at' => $intake->created_at,
                         ]])
                         ->toArray()
