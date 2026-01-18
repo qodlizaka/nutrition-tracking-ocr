b8e50bd461a3a2a9de64962ae969b6aa38388d9f
qodlizaka <qodlizaka513@gmail.com>
2026-01-18 19:10:57 +0700

    make it to capable of displaying multiple dataset

--- resources/views/components/extra/chart/date-range-data-chart.blade.php
+++ resources/views/components/extra/chart/date-range-data-chart.blade.php
@@ -1,13 +1,13 @@
 @props([
-    'data' => [],
+    'datasets' => [],
     'labels' => [],
     'type' => 'line',
-    'title' => 'Default Title',
+    'title' => null,
     'limit' => null,
 ])
 
 @php
-    $chartHash = md5(json_encode([$data, $labels, $limit, $title, $type]));
+    $chartHash = md5(json_encode([$datasets, $labels, $limit, $title, $type]));
 @endphp
 
 <div class="w-full h-full"
@@ -15,7 +15,7 @@
      wire:ignore
      x-data="{
         chart: null,
-        initChart(data, labels, limit, title) {
+        initChart(datasets, labels, limit, title) {
             if (typeof Chart === 'undefined' || typeof twColors === 'undefined') {
                 console.error('Chart.js or twColors is missing');
                 return;
@@ -23,29 +23,62 @@
 
             const ctx = this.$refs.canvas.getContext('2d');
 
-            // No need to destroy old chart, because wire:key ensures
-            // we are always in a fresh DOM element if data changes.
+            const palette = [
+                twColors.blue[600],
+                twColors.red[600],
+                twColors.green[600],
+                twColors.yellow[500],
+                twColors.purple[600],
+                twColors.pink[600],
+                twColors.indigo[600],
+                twColors.orange[500],
+            ];
+
+            const formattedDatasets = datasets.map((ds, index) => {
+                const color = ds.borderColor || palette[index % palette.length];
+                const bgColor = ds.backgroundColor || (typeof hexToRgba === 'function'
+                    ? hexToRgba(color, 0.2)
+                    : color); // Fallback if helper missing
+
+                return {
+                    label: ds.label || `Dataset ${index + 1}`,
+                    data: ds.data,
+                    backgroundColor: bgColor,
+                    borderColor: color,
+                    borderWidth: ds.borderWidth || 2,
+                    // Merge any other chart.js specific options passed in the array
+                    ...ds
+                };
+            });
 
             this.chart = new Chart(ctx, {
                 type: '{{ $type }}',
                 data: {
                     labels: labels,
-                    datasets: [{
-                        label: title,
-                        data: data,
-                        backgroundColor: hexToRgba(twColors.blue[600], 0.2),
-                        borderColor: twColors.blue[600],
-                        borderWidth: 2
-                    }]
+                    datasets: formattedDatasets
                 },
                 options: {
                     responsive: true,
                     maintainAspectRatio: false,
+                    interaction: {
+                        mode: 'index',
+                        intersect: false,
+                    },
                     scales: {
                         x: { grid: { display: false } },
                         y: { grid: { display: false } }
                     },
                     plugins: {
+                        // Enable Legend to differentiate datasets
+                        legend: {
+                            display: true,
+                            position: 'bottom'
+                        },
+                        title: {
+                            display: !!title,
+                            text: title,
+                            font: { size: 16 }
+                        },
                         annotation: {
                             annotations: {
                                 ...(limit !== null ? {
@@ -54,17 +87,17 @@
                                         mode: 'horizontal',
                                         scaleID: 'y',
                                         value: limit,
-                                        borderColor: twColors.blue[600],
+                                        borderColor: twColors.gray[500], // Neutral color for limit
                                         borderWidth: 2,
                                         borderDash: [5, 5],
                                         label: {
                                             display: true,
-                                            content: title + ' limit',
+                                            content: 'Limit: ' + limit,
                                             position: 'end',
-                                            backgroundColor: twColors.blue[500],
+                                            backgroundColor: twColors.gray[600],
                                             color: '#fff',
-                                            font: { size: 12, weight: 'bold' },
-                                            padding: 6,
+                                            font: { size: 10, weight: 'bold' },
+                                            padding: 4,
                                             yAdjust: -10
                                         }
                                     }
@@ -77,7 +110,7 @@
         }
      }"
      x-init='initChart(
-        @json($data, JSON_HEX_APOS),
+        @json($datasets, JSON_HEX_APOS),
         @json($labels, JSON_HEX_APOS),
         @json($limit, JSON_HEX_APOS),
         @json($title, JSON_HEX_APOS)
