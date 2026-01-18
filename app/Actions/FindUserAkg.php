a84ff2455c657b3450e6aedb7b52db9b69265d86
qodlizaka <qodlizaka513@gmail.com>
2026-01-18 21:51:22 +0700

    create action to find user current akg

---
+++ app/Actions/FindUserAkg.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Akg;
+use App\Models\User;
+
+class FindUserAkg
+{
+    /**
+     * Invoke the class instance.
+     */
+    public function __invoke(User $user): Akg
+    {
+        return Akg::query()
+            ->where('min_age', '<', $user->date_of_birth->age)
+            ->where('max_age', '>', $user->date_of_birth->age)
+            ->where('gender', $user->gender)
+            ->first();
+    }
+}
