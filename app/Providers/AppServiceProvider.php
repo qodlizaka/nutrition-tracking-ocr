5799ba55122548725e0bb4d97c2891169fd2ec0f
qodlizaka <qodlizaka513@gmail.com>
2026-01-18 19:11:09 +0700

    add carbon macro for greeting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +24,16 @@ class AppServiceProvider extends ServiceProvider
         Paginator::defaultView('vendor.pagination.tailwind');
 
         Paginator::defaultSimpleView('vendor.pagination.simple-tailwind');
+
+        Carbon::macro('greet', function() {
+            $hour = now()->hour;
+                if ($hour < 12) {
+                    return __('Good Morning');
+                } elseif ($hour < 18) {
+                    return __('Good Afternoon');
+                } else {
+                    return __('Good Evening');
+                }
+        });
     }
 }
