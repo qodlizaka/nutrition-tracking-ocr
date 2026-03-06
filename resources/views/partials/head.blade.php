bf830bf2082cd02d3aab0177a8e85e9dce4d25b6
qodlizaka <qodlizaka513@gmail.com>
2026-03-06 12:54:45 +0700

    change title structure

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ ($title ?? __('No title')) . config('app.name') }}</title>
+<title>{{ ($title ?? __('No title')) }} - {{ config('app.name') }}</title>
 
 <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ date('YmdHis') }}" sizes="any" type="image/x-icon">
 <link rel="icon" href="{{ asset('favicon.svg') }}?v={{ date('YmdHis') }}" type="image/svg+xml">
