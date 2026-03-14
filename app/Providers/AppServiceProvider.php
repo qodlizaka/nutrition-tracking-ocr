ca0ccd2727a61a5086bc8b90107eb6d1d72b9805
qodlizaka <qodlizaka513@gmail.com>
2026-03-14 20:26:07 +0700

    mock api result on local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Actions\ExtractNutritionFactsAction;
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
@@ -13,7 +14,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local')) {
+            $this->app->bind(ExtractNutritionFactsAction::class, fn() => new class {
+                public function __invoke($imageBase64, $mimeType = 'image/jpeg')
+                {
+                    return config('gemini.schemas.sample');
+                }
+            });
+        }
     }
 
     /**
