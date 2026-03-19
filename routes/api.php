8b081c1cb56696ec6ff1470eb2138a394b37ce8f
qodlizaka <qodlizaka513@gmail.com>
2026-03-19 18:51:11 +0700

    create api route to create user for local testing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Actions\FindUserAkg;
+use App\Enum\PhysicalActivityLevel;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+if (app()->environment('local')) {
+    Route::post('/_test/setup-user', function () {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
+
+        $user->userDetails()
+            ->updateOrCreate([
+                'akg_id' => (new FindUserAkg())($user)->id,
+                'weight' => rand(50, 100),
+                'height' => rand(150, 200),
+                'activity_level' => fake()->randomElement(PhysicalActivityLevel::cases())->value,
+            ]);
+
+        return response()->json([
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+    });
+}
